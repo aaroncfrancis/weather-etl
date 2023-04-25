7a8ac7fc48cd6603bdad3f2dd810c95b711f36a9
Aaron Francis <aaronfrancis99@gmail.com>
2023-04-25 17:21:34 -0400

    added scheduler
    will run every hour

---
+++ scheduler.py
@@ -0,0 +1,16 @@
+import schedule
+import time
+import os
+
+def run_main_py():
+    # Replace the file path below with the actual file path of your main.py script
+    file_path = r'C:\Users\aaron\Documents\GitHub\tourism-etl\main.py'
+    os.system(f'python {file_path}')
+
+# Scheduling the main.py script to run every hour
+schedule.every().hour.do(run_main_py)
+
+while True:
+    # Run the scheduled tasks
+    schedule.run_pending()
+    time.sleep(3600)
